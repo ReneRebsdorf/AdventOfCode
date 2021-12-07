e28aef7969539cc0cee16e5efa569f09f6a29bd5
Rene Rebsdorf <renerebsdorf@gmail.com>
2021-12-07 12:54:45 +0100

    add step 2

---
+++ 2021/7/2.ps1
@@ -0,0 +1,30 @@
+# $data = "16,1,2,0,4,2,7,1,2,14"
+$data = Get-Content "input.txt"
+$arr = [int[]]$data.split(",")
+
+# Find highest position
+$highestPosition = 0
+for ($i = 0; $i -lt $arr.Count; $i++) {
+    if ($arr[$i] -gt $highestPosition) {
+        $highestPosition = $arr[$i]
+    }
+}
+
+$lowestFuelCost = [int]::MaxValue
+$cheapestPosition = [int]::MaxValue
+# Find positions to loop through
+for ($i = 0; $i -le $highestPosition; $i++) {
+    # Determine fuel cost for this position
+    $fuelCost = 0
+    for ($j = 0; $j -lt $arr.Count; $j++) {
+        $distance = [System.Math]::Abs($i - $arr[$j])
+        $fuelCost += $distance * ($distance+1) / 2
+    }
+    if ($fuelCost -lt $lowestFuelCost) {
+        $lowestFuelCost = $fuelCost
+        $cheapestPosition = $i
+    }
+}
+"Highest Position: $highestPosition"
+"Least fuel used: $lowestFuelCost"
+"Cheapest position: $cheapestPosition"
